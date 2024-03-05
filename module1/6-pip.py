e8361e0f4cb062d5372d96abe32aff2fd0ac2937
Diego Herrera <psystingod@gmail.com>
2024-03-05 15:47:51 -0600

    pip continued

---
+++ module1/6-pip.py
@@ -0,0 +1,10 @@
+# pip help
+# pip help install
+
+#pip show [package]
+
+# pip install pygame // asume que somos administradores
+# pip install --user pygame // para no administradores y no instalar a todos los usuarios.
+
+# pip install -U package // actualiza un paquete a su ultima version.
+# pip install pygame=1.9.2 instala una version especifica.
